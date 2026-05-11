627afa86fac0adc5f1f5e1cc2dd97c0f576d2551
Maxime Leherquer <m.leherquer-ext@genybet.fr>
2026-05-11 11:54:11 +0200

    update conf

--- dot_config/zsh/private_env.zsh
+++ dot_config/zsh/private_env.zsh
@@ -2,12 +2,3 @@ export ANDROID_HOME="$HOME/Library/Android/sdk"
 export CPPFLAGS="-I/opt/homebrew/opt/openjdk@11/include"
 export LESS='-R'
 export PNPM_HOME="$HOME/Library/pnpm"
-
-eval "$(fnm env --use-on-cd)"
-
-source /opt/homebrew/opt/chruby/share/chruby/chruby.sh
-
-source /opt/homebrew/opt/chruby/share/chruby/auto.sh
-
-chruby ruby-3.4.4
-
